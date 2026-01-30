11c12dc5d148fcfeb11e4c1f27d6eb0842642c66
Tomas Coufal <tcoufal@redhat.com>
2026-01-30 22:09:59 +0100

    feat: adjust gifts section
    Signed-off-by: Tomas Coufal <tcoufal@redhat.com>

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -117,7 +117,7 @@ export const WEDDING_INFO: WeddingInfo = {
     },
     {
       description:
-        "Netoužíme po věcných darech. Vaše přítomnost je pro nás dar největší. Pokud na tom ale budete trvat, budeme rádi, když nám přispějete na rekonstrukci bytu.",
+        "Netoužíme po věcných darech. Vaše přítomnost je pro nás dar největší. Pokud byste nás ale přece jen chtěli obdarovat, potěšíte nás obrázkem nebo třeba příspěvkem na rekonstrukci bytu.",
       icon: "present",
       name: "Dary",
     },
